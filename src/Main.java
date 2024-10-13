502200de728b83423d3a4a259d1884a50683c6b8
Kirill-98 <kirill10307060@gmail.com>
2024-10-13 15:18:29 +0300

    HW

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,65 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        //1 задача
+        int [] numbers = new int[] {1, 2, 3};
+        float [] numbersOne = {1.57f, 7.654f, 9.986f};
+        int [] numberTwo = {2, 4, 6, 8, 10, 12, 14, 16};
+        //2 задача
+        for (int i = 0; i < numbers.length ; i++){
+            System.out.print(numbers[i]);
+            if (i < numbers.length - 1){
+                System.out.print(",");
+            }
+        }
+        System.out.println();
+        for (int i = 0 ; i < numbersOne.length; i++){
+            System.out.print(numbersOne[i]);
+            if (i < numbersOne.length - 1){
+                System.out.print(",");
+            }
+        }
+        System.out.println();
+        for (int i = 0; i < numberTwo.length; i++){
+            System.out.print(numberTwo[i]);
+            if (i < numberTwo.length - 1){
+                System.out.print(",");
+            }
+        }
+        System.out.println();
+        //3 задача
+        for (int i = numbers.length - 1; i >= 0; i--){
+            System.out.print(numbers[i]);
+            if(i > 0){
+                System.out.print(",");
+            }
+        }
+        System.out.println();
+        for (int i = numbersOne.length - 1; i >= 0; i--){
+            System.out.print(numbersOne[i]);
+            if (i > 0){
+                System.out.print(",");
+            }
+        }
+        System.out.println();
+        for (int i = numberTwo.length - 1; i >= 0; i--){
+            System.out.print(numberTwo[i]);
+            if (i > 0){
+                System.out.print(",");
+            }
+        }
+        System.out.println();
+        //4 задача
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        int[] var = new int[]{1, 2, 3};
+        for (int i = 0; i < var.length; i++) {
+            if (var[i] % 2 != 0) {
+                var[i] = var[i] + 1;
+
+            }
+            System.out.print(var[i] + "  ");
         }
     }
 }
